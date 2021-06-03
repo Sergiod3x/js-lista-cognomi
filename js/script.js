0ba117b056300bd2ea0e4845d462ccc6795c407d
Sergiod3x <sergiod3x.@gmail.com>
2021-06-03 17:23:45 +0200

    wip

--- js/script.js
+++ js/script.js
@@ -1,32 +1,22 @@
 var output = document.getElementById('out');
-var num = 0;
 var cognomi =["Musazzi", "ginnini", "conte", "Perda", "Ruffini", "Mastella"]
 var mioCognome = prompt("Inserisci il tuo cognome");
 cognomi.push(mioCognome);
-var upper = "";
-var cognome = "";
-
-
-
-
-
-
-
 
 for (var i = 0; i <=cognomi.length; i++) {
     cognome=cognomi[i];
     var tmp=cognome.toLocaleUpperCase();
     cognomi[i] = tmp;
+    output.innerHTML +=  cognomi[i] + "<br>";
 }
 
-output.innerHTML +=  cognome;
 cognomi.sort();
-cognomi.indexOf("mioCognome")
 
+for (var i = 0; i <=cognomi.length; i++) {
+    output.innerHTML +=  cognomi[i] + "<br>";
+}
 
-
-
-
+output.innerHTML +=(cognomi.indexOf("mioCognome")+1);
 
 
 
